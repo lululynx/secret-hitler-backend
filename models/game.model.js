66af849b4d4773f0c8d29da7755434c2ab87f481
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-08 18:28:46 +0200

    feat: expand game model

--- models/game.model.js
+++ models/game.model.js
@@ -2,18 +2,27 @@
 
 exports.getInitialGameState = () => {
   return {
-    turnCount: 0,
     numberOfLiberals: 0,
     numberOfFascists: 0,
     numberOfLiberalPolicies: 0,
     numberOfFascistPolicies: 0,
+    playerRoleAcknowledgedCount: 0,
+    otherFascistsAcknowledgedCount: 0,
+    presidentAcknowledgedCount: 0,
+    chancellorAcknowledgedCount: 0,
+    chosenPolicyAcknowledgedCount: 0,
+    vetoPowerAcknowledgedCount: 0,
+    turnCount: 0,
+    hitler: undefined,
     currentPresident: undefined,
+    currentChancellor: undefined,
     suggestedChancellor: null,
     voteCount: 0,
-    currentChancellor: undefined,
-    hitler: undefined,
+    eligiblePolicies: [undefined, undefined, undefined],
     electionFailCount: 0,
     vetoPowerUnlocked: false,
     gameOver: false,
+    winningFaction: undefined,
+    askPresidentToExecutePlayer: true,
   }
 }
