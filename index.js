0a8d97a17a75cfc6aceab31c5cec706441141dbc
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-08 11:00:52 +0200

    refactor: outsource socket handler from index

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
 'use strict';
 
-const server = require('http').createServer();
-
 require('dotenv').config();
 
+const server = require('http').createServer();
 const io = require('socket.io')(server);
 
-const gameController = require('./controllers/game.controller');
-const playerController = require('./controllers/player.controller');
-
-io.on('connection', (client) => {
-  console.log(`connected to client ${client.id}`);
-
-  // game methods
-  client.on('createGame', (user) => {
-    console.log('create game with id ', client.id);
-    const playerList = gameController.createGame(client.id, user);
-    client.join(client.id);
-    io.to(client.id).emit('gameCreated', playerList);
-  });
-
-  client.on('joinGame', (gameId, user) => {
-    client.join(gameId);
-    const playerList = gameController.joinGame(gameId, user);
-    io.to(gameId).emit('playerJoinedGame', playerList);
-  });
-
-  client.on('startGame', (gameId) => {
-    const game = gameController.startGame(gameId);
-    io.to(game.id).emit('gameStarted', game);
-  });
-
-  client.on('leaveGame', (user, gameId) => {
-    io.to(gameId).emit('playerLeftGame', gameController.leaveGame());
-  });
-
-  // player methods
-  client.on('voteOnChancellor', playerController.voteOnChancellor);
-  client.on('suggestChancellor', playerController.suggestChancellor);
-  client.on('pickPolicies', playerController.pickPolicies);
-  client.on('executePlayer', playerController.executePlayer);
-  client.on('vetoPolicy', playerController.vetoPolicy);
-
-  client.on('disconnect', () => {
-    console.log(`client ${client.id} disconnected`);
-  })
-
-  client.on('error', (err) => {
-    console.log('received error from client:', client.id);
-    console.log(err);
-  })
-})
+require('./socket.handler').socketHandler(io);
 
 if (!module.parent) {
   const ip = 'localhost';
