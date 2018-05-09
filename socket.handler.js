c5bc7c6f4584c0ddc6a7f7af60d8cee1f0ec4a56
oscmag <oscarbmagnusson@gmail.com>
2018-05-09 11:43:04 +0200

    feat: implement socket.io connection for action methods

--- socket.handler.js
+++ socket.handler.js
@@ -5,39 +5,65 @@ const io = require('./index');
 const metaController = require('./controllers/meta.controller');
 const actionController = require('./controllers/action.controller');
 
+const games = metaController.games;
+
 exports.socketHandler = (io) => {
   io.on('connection', (client) => {
     console.log(`connected to client ${client.id}`);
 
-    // game methods
+    // meta methods
     client.on('createGame', (user) => {
       console.log('created game with id ', client.id);
-      const game = metaController.createGame(client.id, user);
       client.join(client.id);
-      io.to(client.id).emit('gameCreated', game);
+      io.to(client.id).emit('gameCreated', metaController.createGame(client.id, user));
     });
 
     client.on('joinGame', (gameId, user) => {
       client.join(gameId);
-      const game = metaController.joinGame(gameId, user);
-      io.to(gameId).emit('playerJoinedGame', game);
+      io.to(gameId).emit('playerJoinedGame', metaController.joinGame(gameId, user));
     });
 
     client.on('startGame', (gameId) => {
-      const game = metaController.startGame(gameId);
-      io.to(gameId).emit('gameStarted', game);
+      io.to(gameId).emit('gameStarted', metaController.startGame(gameId));
     });
 
     client.on('leaveGame', (gameId, user) => {
       io.to(gameId).emit('playerLeftGame', metaController.leaveGame());
     });
 
-    // player methods
-    client.on('suggestChancellor', actionController.suggestChancellor);
-    client.on('voteOnChancellor', actionController.voteOnChancellor);
-    client.on('pickPolicies', actionController.pickPolicies);
-    client.on('executePlayer', actionController.executePlayer);
-    client.on('vetoPolicy', actionController.vetoPolicy);
+
+    // action methods
+    client.on('messageFromFrontEnd', (message, payload) => {
+      const {gameId, playerId, vote, rejectedPolicy} = payload;
+      const game = games[gameId];
+      if (!game) io.to(gameId).emit('gameNotFound', 'No game found with id ' + gameId);
+
+      switch (message) {
+        case 'acknowledgePlayerRole':
+          io.to(gameId).emit('newGameState', actionController.acknowledgePlayerRole(game, message));
+        case 'acknowledgeOtherFascists':
+          io.to(gameId).emit('newGameState', actionController.acknowledgeOtherFascists(game, message));
+        case 'acknowledgePresident':
+          io.to(gameId).emit('newGameState', actionController.acknowledgePresident(game, message));
+        case 'suggestChancellor':
+          io.to(gameId).emit('newGameState', actionController.suggestChancellor(game, playerId));
+        case 'voteOnChancellor':
+          io.to(gameId).emit('newGameState', actionController.voteOnChancellor(game, playerId, vote));
+        case 'acknowledgeChancellor':
+          io.to(gameId).emit('newGameState', actionController.acknowledgeChancellor(game, message));
+        case 'pickPolicies':
+          io.to(gameId).emit('newGameState', actionController.pickPolicies(game, playerId, rejectedPolicy));
+        case 'acknowledgeChosenPolicy':
+          io.to(gameId).emit('newGameState', actionController.acknowledgeChosenPolicy(game, message));
+        case 'executePlayer':
+          io.to(gameId).emit('newGameState', actionController.executePlayer(game, playerId));
+        case 'chancellorVetoPolicy':
+          io.to(gameId).emit('newGameState', actionController.chancellorVetoPolicy(game, playerId));
+        case 'presidentVetoPolicy':
+          io.to(gameId).emit('newGameState', actionController.presidentVetoPolicy(game, playerId, veto));
+      }
+    })
+
 
     client.on('disconnect', () => {
       console.log(`client ${client.id} disconnected`);
