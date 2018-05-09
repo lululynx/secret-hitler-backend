1d7f375c310d8c8572fbcde79cf9c59b3817a3e2
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-09 16:14:19 +0200

    feat: implement player model as class

--- models/player.model.js
+++ models/player.model.js
@@ -1,13 +1,11 @@
-'use strict';
-
-exports.createPlayer = (user) => {
-  return {
-    user: user,
-    faction: undefined,
-    hitler: false,
-    president: false,
-    chancellor: false,
-    executed: false,
-    chancellorVote: null,
+exports.Player = class Player {
+  constructor(user) {
+    this.user = user;
+    this.faction = undefined;
+    this.hitler = false;
+    this.president = false;
+    this.chancellor = false;
+    this.executed = false;
+    this.chancellorVote = null;
   }
 }
