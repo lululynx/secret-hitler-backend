cffaabfbe3290e2f7038fb8af9cd615ecedc238a
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-10 15:23:37 +0200

    refactor: use game model methods in meta controller

--- controllers/meta.controller.js
+++ controllers/meta.controller.js
@@ -1,27 +1,24 @@
 'use strict';
 
-const GameList = require('../models/gameList.model');
-const createPlayer = require('../models/player.model').createPlayer;
-
-const games = exports.games = new GameList();
+const Game = require('../models/game.model').Game;
+const Player = require('../models/player.model').Player;
 
 exports.createGame = (clientId, user) => {
   return new Game(clientId, user);
 }
 
-exports.joinGame = (game, user) => {
-  const player = createPlayer(user);
-  game.playerList.push(player);
+exports.joinGame = ({game, user}) => {
+  const player = new Player(user);
+  game.addPlayer(player);
 }
 
-exports.startGame = (game) => {
+exports.startGame = ({game}) => {
   game.setRoles();
   game.assignPlayersFactions();
-  actionHelpers.drawThreePolicies(game);
-  game.message = 'showRoles';
+  game.drawThreePolicies();
+  game.setMessage('showRoles');
 }
 
-exports.leaveGame = (game, user) => {
-  const index = game.playerList.findIndex(player => player.user.id === user.id);
-  game.playerList.splice(index, 1);
+exports.leaveGame = ({game, user}) => {
+  game.removePlayer(player);
 }
