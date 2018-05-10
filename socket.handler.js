eaf29a524ce2944fd1e7b93837da587c29a44c33
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-10 18:37:36 +0200

    fix: socket handling and channel naming

--- socket.handler.js
+++ socket.handler.js
@@ -12,21 +12,21 @@ exports.socketHandler = (io) => {
 
     // meta methods
     client.on('metaChannel', (message, payload) => {
-      console.log(message, payload);
-      validatePayload(client, payload);
+      const err = validatePayload(client, payload);
+      if (err) return client.emit('error', err);
+      payload.clientId = client.id;
       if (message === 'createGame') client.join(client.id);
       else if (message === 'joinGame') client.join(payload.gameId);
       else if (message === 'leaveGame') client.leave(payload.gameId);
       metaController[message](payload);
-      io.in(payload.gameId).emit('metaChannel', payload.game);
+      io.in(payload.game.id).emit('metaChannel', payload.game);
     });
 
-
     // action methods
-    client.on('messageFromFrontEnd', (message, payload) => {
+    client.on('gameChannel', (message, payload) => {
       validatePayload(payload);
       actionController[message](payload);
-      io.in(payload.gameId).emit('newGameState', payload.game);
+      io.in(payload.gameId).emit('gameChannel', payload.game);
     })
 
     client.on('disconnect', () => {
