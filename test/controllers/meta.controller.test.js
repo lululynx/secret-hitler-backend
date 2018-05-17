620e15392336298c709fbf04bd3760f91ec05b89
oscmag <oscarbmagnusson@gmail.com>
2018-05-17 23:14:35 +0200

    test: implement test for started game properties

--- test/controllers/meta.controller.test.js
+++ test/controllers/meta.controller.test.js
@@ -39,50 +39,104 @@ const disconnectClients = (clients) => {
 }
 
 
-describe.only('Sockets', function () {  
-  let clients = [];
-  let currentGame;
+describe.only('Sockets', () => {  
+  let game;
 
-  after(() => {
-    server.close();
-  })
-
-  afterEach(done => {
-    disconnectClients(clients);
-    done();
-  });
-
-  it('should send a game object back when type is "createGame"', done => {
-    clients = connectClients(1);
-    clients[0].on('data', data => {
-      currentGame = data.payload;
-      expect(currentGame).to.be.a('object');
+  describe('Meta controllers', () => {
+    let clients = [];
+    after(() => {
+      server.close();
+    })
+  
+    afterEach(done => {
+      disconnectClients(clients);
       done();
     });
-    createGame(clients[0]);
-  });
-
-  it('should be able to join existing game and listen to backend', done => {
-    clients = connectClients(1);
-    clients[0].on('data', data => {
-      currentGame = data.payload;
-      expect(currentGame.playerList.length).to.equal(2);
-      done();
-    });
-    joinGame(clients, currentGame, 1, 1)
-  });
-
-  it('should be able to start game when five or more people', done => {
-    clients = connectClients(4);
-    clients[0].on('data', data => {
-      currentGame = data.payload;
-      if (currentGame.numberOfLiberals) {
-        expect(currentGame.message).to.equal('showRoles');
+  
+    it('should send a game object back when type is "createGame"', done => {
+      clients = connectClients(1);
+      clients[0].on('data', data => {
+        game = data.payload;
+        expect(game).to.be.a('object');
         done();
-      }
+      });
+      createGame(clients[0]);
     });
-    joinGame(clients, currentGame, 3, 2);
-    startGame(clients, currentGame);
+  
+    it('should be able to join existing game and listen to backend', done => {
+      clients = connectClients(1);
+      clients[0].on('data', data => {
+        game = data.payload;
+        expect(game.playerList.length).to.equal(2);
+        done();
+      });
+      joinGame(clients, game, 1, 1)
+    });
+  
+    it('should be able to start game with correct property values', done => {
+      clients = connectClients(4);
+      clients[0].on('data', data => {
+        game = data.payload;
+        if (game.numberOfLiberals) {
+  
+          done();
+        }
+      });
+      joinGame(clients, game, 3, 2);
+      setTimeout(() => {
+        startGame(clients, game);
+      }, 50)
+    });
   });
+
+  describe('Started game properties', () => {
+
+    it('game message should be "showRoles"', () => {
+      game.message.should.equal('showRoles');
+    });
+
+    it('current president should be a name', () => {
+      game.currentPresident.should.be.a('string');
+    });
+
+    it('there should be no suggested chancellor', () => {
+      expect(game.suggestedChancellor).to.equal(null);
+    });
+
+    it('there should be 3 liberals in a game of 5', () => {
+      const actualLiberals = game.playerList.filter(player => player.faction === 'liberal').length;
+      actualLiberals.should.equal(3);
+    });
+
+    it('there should be 2 fascists in a game of 5', () => {
+      const actualFascists = game.playerList.filter(player => player.faction === 'fascist').length;
+      actualFascists.should.equal(2);
+    });
+
+    it('only one should be Hitler', () => {
+      const numberOfHitlers = game.playerList.filter(player => player.hitler).length;
+      numberOfHitlers.should.equal(1);
+    });
+
+    it('the player list array should have 5 players', () => {
+      game.playerList.length.should.equal(5);
+    });
+
+    it('there should be 3 eligible policies', () => {
+      game.eligiblePolicies.length.should.equal(3);
+    });
+
+    it('the player who created the game should be the initiator', () => {
+      game.initiator.name.should.equal(mocks.users[0].name);
+    });
+
+    it('the value of numberOfLiberals property should be 3', () => {
+      game.numberOfLiberals.should.equal(3);
+    });
+
+    it('the value of numberOfFascists property should be 2', () => {
+      game.numberOfFascists.should.equal(2);
+    });
+  })
 });
 
