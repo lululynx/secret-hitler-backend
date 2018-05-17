e4b47ca1ac1839defe86c7c37cafad356c7fd59c
oscmag <oscarbmagnusson@gmail.com>
2018-05-17 21:36:00 +0200

    chore: add mocks for meta controller tests

---
+++ test/mocks.js
@@ -0,0 +1,22 @@
+module.exports.users = [
+  {
+    name: 'Eric',
+    id: 'abc123'
+  },
+  {
+    name: 'Batman',
+    id: 'qwe123'
+  },
+  {
+    name: 'Emma',
+    id: 'qwe523'
+  },
+  {
+    name: 'Martin',
+    id: 'qwe122'
+  },
+  {
+    name: 'Dan',
+    id: 'qwe178'
+  },
+]
